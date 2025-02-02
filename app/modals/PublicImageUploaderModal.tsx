c4ddad742d651345194b027e92ea0d2a949d038e
rowgregory <rowgregory@gmail.com>
2025-02-02 12:08:35 -0500

    chore(image-video-modals):Update ui to match textarea modal

--- app/modals/PublicImageUploaderModal.tsx
+++ app/modals/PublicImageUploaderModal.tsx
@@ -1,16 +1,17 @@
 import React, { ChangeEvent, FC, useEffect, useRef, useState } from 'react'
+import Link from 'next/link'
+import { RootState, useAppDispatch, useAppSelector } from '../redux/store'
 import PublicModal from '../components/common/PublicModal'
 import { useUpdateTextBlockMutation } from '../redux/services/textBlockApi'
+import { setCloseModalImageUploaderPublic } from '../redux/features/appSlice'
+import uploadFileToFirebase from '../utils/uploadFileToFirebase'
+import useSoundEffect from '../hooks/useSoundEffect'
 import useForm from '../hooks/useForm'
-import AwesomeIcon from '../components/common/AwesomeIcon'
-import { arrowRightIcon, timesIcon } from '../icons'
+import EditableTextAreaIcon from '../icons/EditableTextAreaIcon'
 import AppleLoader from '../components/common/AppleLoader'
 import Picture from '../components/common/Picture'
-import Link from 'next/link'
-import uploadFileToFirebase from '../utils/uploadFileToFirebase'
-import { RootState, useAppDispatch, useAppSelector } from '../redux/store'
-import { setCloseModalImageUploaderPublic } from '../redux/features/appSlice'
-import useSoundEffect from '../hooks/useSoundEffect'
+import AwesomeIcon from '../components/common/AwesomeIcon'
+import { arrowRightIcon } from '../icons'
 
 interface PublicImageUploaderModalProps {
   show: boolean
@@ -48,10 +49,10 @@ const PublicImageUploaderModal: FC<PublicImageUploaderModalProps> = ({ show, src
       .unwrap()
       .then(async () => {
         play()
-        setLoading(false)
         setInputs({ src: url, mimeType: 'image' })
       })
       .catch(() => {})
+    setLoading(false)
   }
 
   const reset = () => {
@@ -63,31 +64,21 @@ const PublicImageUploaderModal: FC<PublicImageUploaderModalProps> = ({ show, src
   if (!show) return
 
   return (
-    <PublicModal show={show} onClose={reset} zIndex="z-50">
-      <div className="py-20 max-w-md mx-auto flex flex-col items-center justify-center w-full">
-        <AwesomeIcon icon={timesIcon} onClick={() => reset()} className="w-5 h-5 text-jetBlack absolute top-5 left-5 z-10 cursor-pointer" />
-        <Picture
-          src="/images/upload-image-modal.png"
-          alt="Upload an Image"
-          className="w-14 h-14 text-skyAqua"
-          priority={false}
-          width={80}
-          height={80}
-        />
-        <h1 className="font-poppins font-medium text-jetBlack text-xl mt-5 mb-2"> Upload image</h1>
-        <p className="text-sm text-[#7e7e7e] text-center font-poppins">
+    <PublicModal show={show} onClose={reset} reset={reset}>
+      <div className="px-4 pt-12 480:py-20 480:mb-20 max-w-md mx-auto flex flex-col items-center justify-center w-full">
+        <EditableTextAreaIcon className={`${loading ? 'animate-rotate360' : ''}`} />
+        <h1 className="font-medium text-jetBlack text-xl mt-2 mb-4">Upload image</h1>
+        <p className="hidden 480:block text-sm text-[#7e7e7e] text-center">
           Please note that uploading a new image will automatically delete the previous one and immediately replace it with the new one.
         </p>
-        <p>
-          <Link
-            className="flex items-center gap-x-2 mb-10 mt-1.5"
-            href="https://console.firebase.google.com/u/0/project/thoroughly-cleaned-66710/storage/thoroughly-cleaned-66710.firebasestorage.app/files/~2Fimages"
-            target="_blank"
-          >
-            <span className="text-skyAqua font-poppins text-left w-fit">View All Media Stored on Firebase</span>
-            <AwesomeIcon icon={arrowRightIcon} className="w-4 h-4 text-skyAqua -rotate-45" />
-          </Link>
-        </p>
+        <Link
+          className="flex items-center gap-x-2 mb-10 mt-1.5"
+          href="https://console.firebase.google.com/u/0/project/thoroughly-cleaned-66710/storage/thoroughly-cleaned-66710.firebasestorage.app/files/~2Fimages"
+          target="_blank"
+        >
+          <span className="text-skyAqua w-fit text-sm">View All Media Stored on Firebase</span>
+          <AwesomeIcon icon={arrowRightIcon} className="w-4 h-4 text-skyAqua -rotate-45" />
+        </Link>
         <div className="border-1 border-dashed border-zinc-700 w-full p-5 h-44 flex flex-col items-center justify-center cursor-pointer hover:border-skyAqua bg-zinc-50">
           {!loading ? (
             <>
@@ -95,7 +86,7 @@ const PublicImageUploaderModal: FC<PublicImageUploaderModalProps> = ({ show, src
                 onClick={() => mediaInputRef?.current?.click()}
                 src={inputs.src || '/images/logo.png'}
                 alt="Upload Image"
-                className="w-full h-full max-w-40 object-contain"
+                className="w-full h-full max-w-60 object-contain"
                 priority={true}
                 width={100}
                 height={100}
@@ -109,22 +100,24 @@ const PublicImageUploaderModal: FC<PublicImageUploaderModalProps> = ({ show, src
           )}
         </div>
       </div>
-      <div className="bg-[#cfcfcf] mx-auto max-w-md 990:max-w-full py-6 px-5 flex items-center justify-between w-full">
-        <button
-          onClick={() => reset()}
-          type="button"
-          className="bg-[#333336] hover:bg-[#38383c] py-1.5  w-36  font-poppins text-white disabled:cursor-not-allowed disabled:bg-zinc-800"
-        >
-          Back
-        </button>
-        <div className="flex items-center gap-x-4">
+      <div className="bg-[#cfcfcf] mx-auto max-w-md 990:max-w-full p-3 480:py-6 480:px-5 fixed bottom-0 left-0 right-0 480:block w-full">
+        <div className="flex items-center justify-between">
           <button
             onClick={() => reset()}
             type="button"
-            className="hover:bg-neonSkyAqua bg-skyAqua py-1.5 w-36 font-poppins text-white disabled:cursor-not-allowed disabled:bg-sky-700"
+            className="bg-[#333336] hover:bg-[#38383c] py-1.5 w-36 text-white disabled:cursor-not-allowed"
           >
-            Done
+            Back
           </button>
+          <div className="flex items-center gap-x-4">
+            <button
+              onClick={() => reset()}
+              type="button"
+              className="min-w-36 bg-skyAqua py-1.5 w-36 text-white disabled:cursor-not-allowed"
+            >
+              Done
+            </button>
+          </div>
         </div>
       </div>
     </PublicModal>
--- app/modals/PublicVideoUploaderModal.tsx
+++ app/modals/PublicVideoUploaderModal.tsx
@@ -1,16 +1,16 @@
 import React, { ChangeEvent, FC, useEffect, useRef, useState } from 'react'
-import PublicModal from '../components/common/PublicModal'
-import { useUpdateTextBlockMutation } from '../redux/services/textBlockApi'
-import useForm from '../hooks/useForm'
-import AwesomeIcon from '../components/common/AwesomeIcon'
-import { arrowRightIcon, timesIcon } from '../icons'
-import AppleLoader from '../components/common/AppleLoader'
-import Picture from '../components/common/Picture'
 import Link from 'next/link'
-import uploadFileToFirebase from '../utils/uploadFileToFirebase'
 import { useAppDispatch } from '../redux/store'
+import { useUpdateTextBlockMutation } from '../redux/services/textBlockApi'
+import uploadFileToFirebase from '../utils/uploadFileToFirebase'
 import { setCloseModalEditableVideoPublic } from '../redux/features/appSlice'
-import ReactPlayer from 'react-player'
+import useForm from '../hooks/useForm'
+import PublicModal from '../components/common/PublicModal'
+import EditableTextAreaIcon from '../icons/EditableTextAreaIcon'
+import Spinner from '../components/common/Spinner'
+import AppleLoader from '../components/common/AppleLoader'
+import AwesomeIcon from '../components/common/AwesomeIcon'
+import { arrowRightIcon } from '../icons'
 
 interface PublicVideoUploaderModalProps {
   show: boolean
@@ -59,63 +59,54 @@ const PublicVideoUploaderModal: FC<PublicVideoUploaderModalProps> = ({ show, src
     setInputs({})
     setLoading(false)
   }
+  const [videoLoaded, setVideoLoaded] = useState(false)
+
+  const handleVideoLoad = () => {
+    setVideoLoaded(true)
+  }
 
   if (!show) return
 
   return (
-    <PublicModal show={show} onClose={reset}>
-      <div className="py-20 max-w-md mx-auto flex flex-col items-center justify-center h-full w-full">
-        <AwesomeIcon icon={timesIcon} onClick={() => reset()} className="w-5 h-5 text-jetBlack absolute top-5 left-5 z-10 cursor-pointer" />
-        <Picture
-          src="/images/upload-image-modal.png"
-          alt="Upload an Image"
-          className="w-14 h-14 text-skyAqua"
-          priority={false}
-          width={80}
-          height={80}
-        />
-        <h1 className="font-poppins font-medium text-jetBlack text-xl mt-5 mb-2"> Upload video</h1>
-        <p className="text-sm text-[#7e7e7e] text-center font-poppins">
+    <PublicModal show={show} onClose={reset} reset={reset}>
+      <div className="px-4 pt-12 480:py-20 480:mb-20 max-w-md mx-auto flex flex-col items-center justify-center w-full">
+        <EditableTextAreaIcon className={`${loading ? 'animate-rotate360' : ''}`} />
+        <h1 className="font-medium text-jetBlack text-xl mt-2 mb-4">Upload video</h1>
+        <p className="hidden 480:block text-sm text-[#7e7e7e] text-center">
           Please note that uploading a new video will automatically delete the previous one and immediately replace it with the new one.
         </p>
-        <p>
-          <Link
-            className="flex items-center gap-x-2 mb-10 mt-1.5"
-            href="https://console.firebase.google.com/u/0/project/thoroughly-cleaned-66710/storage/thoroughly-cleaned-66710.firebasestorage.app/files/~2Fimages"
-            target="_blank"
-          >
-            <span className="text-skyAqua font-poppins text-left w-fit">View All Media Stored on Firebase</span>
-            <AwesomeIcon icon={arrowRightIcon} className="w-4 h-4 text-skyAqua -rotate-45" />
-          </Link>
-        </p>
+        <Link
+          className="flex items-center gap-x-2 mb-10 mt-1.5"
+          href="https://console.firebase.google.com/u/0/project/thoroughly-cleaned-66710/storage/thoroughly-cleaned-66710.firebasestorage.app/files/~2Fimages"
+          target="_blank"
+        >
+          <span className="text-skyAqua w-fit text-sm">View All Media Stored on Firebase</span>
+          <AwesomeIcon icon={arrowRightIcon} className="w-4 h-4 text-skyAqua -rotate-45" />
+        </Link>
         <div className="border-1 border-dashed border-zinc-700 w-full p-5 h-44 flex flex-col items-center justify-center cursor-pointer hover:border-skyAqua bg-zinc-50">
           {!loading ? (
-            <div onClick={() => mediaInputRef?.current?.click()} className="max-w-40">
-              <ReactPlayer
-                // onReady={() => dispatch(setIsMediaReady())}
-                style={{
-                  objectFit: 'cover',
-                  width: '100%',
-                  height: 'auto',
-                  zIndex: 10,
-                  maxWidth: '160px'
-                }}
-                playing={false}
-                url={inputs.src}
-                muted={true}
-                loop={true}
-                controls={true}
-                width={300}
-                height={170}
-                config={{
-                  file: {
-                    attributes: {
-                      preload: 'auto',
-                      playsInline: true
-                    }
-                  }
-                }}
-              />
+            <div onClick={() => mediaInputRef?.current?.click()} className="max-w-60 relative">
+              <video
+                src={inputs.src}
+                className="object-cover w-full h-auto z-10 max-w-60 relative"
+                controls={false}
+                autoPlay={false}
+                loop={false}
+                muted
+                preload="metadata"
+                onCanPlay={handleVideoLoad}
+              >
+                <source src={src} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+              {!videoLoaded && (
+                <Spinner
+                  fill="fill-skyAqua"
+                  wAndH="w-5 h-5"
+                  className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                />
+              )}
+
               <input ref={mediaInputRef} type="file" className="hidden" onChange={handleFileUpload} accept="video/*" />
             </div>
           ) : (
@@ -125,22 +116,24 @@ const PublicVideoUploaderModal: FC<PublicVideoUploaderModalProps> = ({ show, src
           )}
         </div>
       </div>
-      <div className="bg-[#cfcfcf] py-6 px-5 flex items-center justify-between w-full">
-        <button
-          onClick={() => reset()}
-          type="button"
-          className="bg-[#333336] hover:bg-[#38383c] py-1.5  w-36  font-poppins text-white disabled:cursor-not-allowed disabled:bg-zinc-800"
-        >
-          Back
-        </button>
-        <div className="flex items-center gap-x-4">
+      <div className="bg-[#cfcfcf] mx-auto max-w-md 990:max-w-full p-3 480:py-6 480:px-5 fixed bottom-0 left-0 right-0 480:block w-full">
+        <div className="flex items-center justify-between">
           <button
             onClick={() => reset()}
             type="button"
-            className="hover:bg-neonSkyAqua bg-skyAqua py-1.5 w-36 font-poppins text-white disabled:cursor-not-allowed disabled:bg-sky-700"
+            className="bg-[#333336] hover:bg-[#38383c] py-1.5 w-36 text-white disabled:cursor-not-allowed"
           >
-            Done
+            Back
           </button>
+          <div className="flex items-center gap-x-4">
+            <button
+              onClick={() => reset()}
+              type="button"
+              className="min-w-36 bg-skyAqua py-1.5 w-36 text-white disabled:cursor-not-allowed"
+            >
+              Done
+            </button>
+          </div>
         </div>
       </div>
     </PublicModal>
